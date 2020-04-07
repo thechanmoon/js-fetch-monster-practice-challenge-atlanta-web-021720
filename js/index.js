4fcd5d1b10aeaadbd8b231e72b1d2b34a1cb1f7f
tiburon <thechanmoon@gmail.com>
2020-04-07 01:19:05 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
-// const btn_back = document.querySelector('#back')
-// const btn_forward = document.querySelector('#forward')
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'http://localhost:3000/'
+let page = 1;
 
 function init(event) {
 
     createNewForm();
+    getData();
+    addNavListeners();
 
-    getData(BASE_URL + '/monsters')
+}
+
+function getData(page) {
+    fetch(BASE_URL + `monsters/?_limit=50&_page=${page}`)
         .then(res => res.json())
         .then(data => {
             //debugger
             //console.log(data)
             parseData(data);
         });
-
-}
-
-function getData(router, option = null) {
-    return fetch(router)
 }
 
 function parseData(array) {
@@ -50,23 +49,70 @@ function createNewForm() {
     let element1 = document.createElement("input");
     let element2 = document.createElement("input");
     let element3 = document.createElement("input");
+    let element4 = document.createElement('button');
 
-
-    form.method = "POST";
+    // form.method = "POST";
+    form.id = 'monster-form'
 
     element1.value = "";
-    element1.name = "name";
+    element1.id = "name";
     form.appendChild(element1);
 
     element2.value = "";
-    element2.name = "age";
+    element2.id = "age";
     form.appendChild(element2);
 
     element3.value = "";
-    element3.name = "decription";
+    element3.id = "description";
     form.appendChild(element3);
 
+
+    element4.innerHTML = 'Create'
+    form.appendChild(element4);
+
     div_create_monster.appendChild(form);
+    addSubmitEventListener()
 }
+
+function addSubmitEventListener(){ 
+    document.querySelector('#monster-form')
+    .addEventListener('submit', 
+    a => { 
+        a.preventDefault(), 
+        console.log('submitted', getFormData()), 
+        postNewData(getFormData()), clearForm() }) 
+}
+
+function getFormData(){
+    let input_name = document.querySelector('#name'),
+        input_age = document.querySelector('#age'),
+        input_descripton = document.querySelector('#description');
+    return { name: input_name.value, age: parseFloat(input_age.value), description: input_descripton.value }
+}
+function postNewData(new_data){
+    let url = BASE_URL + `monsters`,
+        config = { 
+            method: 'POST', 
+            headers: { 'Content-type': 'application/json', 
+            Accept: 'application/json' }, 
+            body: JSON.stringify(new_data) 
+        };
+    fetch(url, config)
+    .then(res => res.json())
+    .then(data => console.log('new monster', data))
+}
+
+function clearForm(){ 
+    document.querySelector('#monster-form').reset() 
+}
+
+function addNavListeners(){
+    let button_back = document.querySelector('#back'),
+        button_forward = document.querySelector('#forward');
+        button_back.addEventListener('click', () => { pageDown() }), 
+        button_forward.addEventListener('click', () => { pageUp() })
+}
+function pageUp(){ page++, getData(page) }
+function pageDown(){ 1 < page ? (page--, getData(page)) : alert('no monsters') }
 
 document.addEventListener('DOMContentLoaded', init);
